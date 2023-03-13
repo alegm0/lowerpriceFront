f8f8ce35adab72b490d8912b87363a02a2f82b40
Alejandra G <maria.gonzalez1.mt@correo.usa.edu.co>
2023-03-12 21:16:16 -0500

    Cambios Login

--- src/View/login/Login.jsx
+++ src/View/login/Login.jsx
@@ -3,7 +3,12 @@ import { useHistory } from "react-router";
 import { Col, Container, Row } from "react-bootstrap";
 import Button from "@restart/ui/esm/Button";
 import loginImg from "../../assets/img/loginImg.svg";
+import loginImgI from "../../assets/img/loginIcon.png";
 import logo from "../../assets/img/logo.svg";
+import user from "../../assets/img/email.png";
+import password from "../../assets/img/forgot.png";
+
+
 
 function Login() {
   const history = useHistory();
@@ -36,67 +41,70 @@ function Login() {
     <Container className="full-width">
       <Row>
         <Col className="background-red col-height">
-          <h1 className="main__title">¡Ingresa ya!</h1>
-          <p className="main__description">
-            Te brindamos el servicio para mejorar tu organización en cada una de
-            tus compras, es importante para nosotros hacer tu vida mucho mas
-            sencilla.
-          </p>
-          <img src={loginImg} alt="Imagen ingreso" className="image-size" />
+
+
+          <img src={loginImgI} alt="Imagen ingreso" className="image-size" />
         </Col>
         <Col className="col-height">
-          <img className="main__image" src={logo} alt="MBShop Logo" />
-          <p className="main__text">Ingrese los siguientes datos</p>
+          <div className="panel-white">
+           
+            <h1>Iniciar Sesion</h1>
+            <p className="main__text">Ingrese los siguientes datos</p>
 
-          <div className="flex-inputs">
-            <label className="label-input">Código estudiantil:</label>
-            <input
-              className="input"
-              type="text"
-              placeholder="Ingrese su código"
-              name="student_code"
-              onChange={(e) => handleChangeInfo(e)}
-              value={userInfo?.student_code || ""}
-            />
-          </div>
-          <div className="flex-inputs last-input-margin">
-            <label className="label-input">Contraseña:</label>
-            <input
-              className="input"
-              type="password"
-              placeholder="Ingrese su contraseña"
-              name="password"
-              onChange={(e) => handleChangeInfo(e)}
-              value={userInfo?.password || ""}
-            />
-          </div>
+            <div className="flex-inputs">
 
-          {validate && (
-            <div>
-              <p>*Usuario o contraseña incorrecta</p>
+              <img src={user} alt="Imagen ingreso" className="input-icon" />
+              <input
+                className="input"
+                type="text"
+                placeholder="Ingrese su correo"
+                name="student_code"
+                onChange={(e) => handleChangeInfo(e)}
+                value={userInfo?.student_code || ""}
+              />
             </div>
-          )}
 
-          <div>
-            <a
-              href="/recover-password"
-              className="login__recover-password-link"
+            <div className="flex-inputs last-input-margin">
+            <img src={password} alt="Imagen ingreso" className="input-icon" />
+
+              <input
+                className="input"
+                type="password"
+                placeholder="Ingrese su contraseña"
+                name="password"
+                onChange={(e) => handleChangeInfo(e)}
+                value={userInfo?.password || ""}
+              />
+            </div>
+
+            {validate && (
+              <div>
+                <p>*Usuario o contraseña incorrecta</p>
+              </div>
+            )}
+
+      
+            <Button
+              variant="INICIAR SESIÓN"
+              className="button-red"
+              onClick={() => login()}
             >
-              ¿Olvidaste tu contraseña?
-            </a>
+              INICIAR SESIÓN
+            </Button>
+            <div>
+              <a
+                href="/recover-password"
+                className="login__recover-password-link"
+              >
+                ¿Olvidaste tu contraseña?
+              </a>
+            </div>
+
+
+            <p className="login__without-account">
+              ¿No tienes una cuenta? <a href="/register">registrate aquí</a>.
+            </p>
           </div>
-
-          <Button
-            variant="INICIAR SESIÓN"
-            className="button-red"
-            onClick={() => login()}
-          >
-            INICIAR SESIÓN
-          </Button>
-
-          <p className="login__without-account">
-            Si no tienes una cuenta <a href="/register">registrate aquí</a>.
-          </p>
         </Col>
       </Row>
     </Container>
