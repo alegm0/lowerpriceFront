7c4ea2ee8e7fd100a3b61c728c7e3ef3fb571cb5
John Doe <johndoe@example.com>
2023-05-03 15:12:14 -0500

    Cambios de funcionalidad discounts

--- src/View/my-products/SetDiscounts.jsx
+++ src/View/my-products/SetDiscounts.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import moment from 'moment';
 import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import { useHistory } from "react-router";
 import { urlRequest } from "../../urlRequest";
@@ -14,24 +15,87 @@ import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
 function SetDiscounts() {
-    // const [selectedDate, setSelectedDate] = useState(null);
 
-    // const handleDateChange = (date) => {
-    //     setSelectedDate(date);
-    // };
+    const FORMAT = "dd-MM-yyyy"
+
+    const validateInputs = {
+
+        start_date: false,
+        finish_date: false,
+        value: false,
+        conditions: false
+    }
 
 
+    const history = useHistory();
     const [selectedDate, setSelectedDate] = useState(new Date());
     const [selectedDateFinal, setSelectedDateFinal] = useState(new Date());
 
-    const handleDateChange = (date) => {
+    const [errorsInputs, setErrorsInputs] = useState({ ...validateInputs });
+    const [discounts, setDiscounts] = useState({
+        start_date: moment(selectedDate).format('YYYY-MM-DD'),
+        finish_date: moment(selectedDateFinal).format('YYYY-MM-DD'),
+        value: '',
+        conditions: ''
+    });
+    const handleSelectedDateChange = (date) => {
         setSelectedDate(date);
+        setDiscounts({ ...discounts, start_date: moment(date).format('YYYY-MM-DD') });
     };
-    const history = useHistory();
+
+    const handleSelectedDateFinalChange = (date) => {
+        setSelectedDateFinal(date);
+        setDiscounts({ ...discounts, finish_date: moment(date).format('YYYY-MM-DD') });
+    };
+    const onChange = (e) => {
+        setDiscounts({ ...discounts, [e.target.name]: e.target.value });
+    }
+
+
+
+
+
+
+
+
+    const validate = () => {
+
+        const errors = { ...validateInputs };
+
+        Object.keys(errors).forEach((e) => {
+            errors[e] = !discounts[e] ? '*Campo es obligatorio' : '';
+        });
+
+
+        setErrorsInputs(errors);
+        return Object.values(errors).some(x => x);
+    }
+
+
+    //aaaaaaaaa
+    const onSubmit = () => {
+        // if (!validate()) {
+        axios.post(`${urlRequest}/discount-promotions`, discounts)
+            .then(function (response) {
+                if (response.status === 201) {
+                    history.push('/discounts');
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+
+        // } else {
+        //     setErrorsInputs("Error ejecutamiento");
+        // }
+    };
+
+
     return (
 
         <div className="body-view">
             <Container>
+
                 <Row>
                     <Col lg={1}>
                         <img onClick={() => history.goBack()} src={iconoAtras} alt="Icono de atras" style={{ width: "3rem", marginTop: "3rem", marginLeft: "-3rem" }} />
@@ -51,51 +115,77 @@ function SetDiscounts() {
                         <h1 className="second-Title">Valor(*)</h1>
                         <input
                             className="inputDiscounts"
-                            type="text"
-                            placeholder="Ingrese su correo electronico"
-                            name="email"
+
+                            type="number"
+                            placeholder="Ingrese el valor"
+                            name="value"
+                            value={discounts.value}
+                            onChange={(e) => onChange(e)}
 
                         />
+                        {errorsInputs.name && <span className="text-validate">{errorsInputs.value}</span>}
+
                         <h1 className="second-Title">Fecha final(*)</h1>
 
                         <DatePicker
+
                             selected={selectedDateFinal}
                             className="inputDiscounts"
-                            onChange={date => setSelectedDateFinal(date)}
+                            dateFormat={FORMAT}
+                            value={discounts.finish_date}
+
+                            onChange={handleSelectedDateFinalChange}
 
                         />
+                        {errorsInputs.name && <span className="text-validate">{errorsInputs.finish_date}</span>}
 
                     </Col>
 
                     <Col>
                         <h1 className="second-Title">Fecha inicial(*)</h1>
                         <DatePicker
+
                             selected={selectedDate}
                             className="inputDiscounts"
-                            onChange={date => setSelectedDate(date)}
+                            onChange={handleSelectedDateChange}
+
+                            // onChange={date => setSelectedDate(date)}
+                            dateFormat={FORMAT}
+                            value={discounts.start_date}
+
 
                         />
+                        {errorsInputs.name && <span className="text-validate">{errorsInputs.start_date}</span>}
                         <h1 className="second-Title">Condiciones</h1>
                         <input
                             className="inputDiscounts"
                             type="text"
                             placeholder="Ingrese las condiciones"
-                            name="email"
+                            name="conditions"
+
+
                             style={{
                                 height:
-                                "200px",
-                                textAlign:"start",
+                                    "200px",
+                                textAlign: "start",
                                 position: "relative"
-                                              }}
+                            }}
+                            value={discounts.conditions}
+                            onChange={(e) => onChange(e)}
 
                         />
+                        {errorsInputs.name && <span className="text-validate">{errorsInputs.conditions}</span>}
 
                     </Col>
                 </Row>
                 <Row>
-                <Button className='buttonSave' href='/discounts'>Guardar</Button>
+                    <Button
+                        type="submit"
+                        className='buttonSave'
+                        onClick={onSubmit}
+                    >Guardar</Button>
 
-          
+
                 </Row>
 
 
